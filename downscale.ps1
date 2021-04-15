79a806fd86172f056b71eb025feb35a3e11f2efe
Reno <33203203+redat00@users.noreply.github.com>
2021-04-15 13:18:13 +0200

    Add files via upload

---
+++ downscale.ps1
@@ -0,0 +1,64 @@
+﻿$VMs = 'g2-web-02','g2-web-03','g2-web-04'
+
+function connect_vsphere
+{
+    Write-Output "Connection to vSphere"
+    Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+    Connect-VIServer -Server 10.54.2.1 -Protocol https -User root -Password 'Pa$$w0rd'
+}
+
+function disconnect_vsphere
+{
+    Disconnect-VIServer -Server 10.54.2.1 -Confirm:$false
+}
+
+function get_ha_conf_version()
+{
+    $credentials_ha = "dataplaneapi:password"
+    $encoded_credentials = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($credentials_ha))
+    $headers = @{ Authorization = "Basic $encoded_credentials" }
+    $Response = Invoke-WebRequest -Uri http://10.54.2.2:5555/v2/services/haproxy/configuration/version -Method GET -Headers $headers
+    $version_ha = $Response.Content
+    $version_ha = $version_ha -as [int]
+    $version_ha
+}
+
+function remove_from_ha($vmname)
+{
+    $version_ha = get_ha_conf_version
+    $credentials_ha = "dataplaneapi:password"
+    $encoded_credentials = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($credentials_ha))
+    $headers = @{ Authorization = "Basic $encoded_credentials" }
+    $Response = Invoke-WebRequest -Uri http://10.54.2.2:5555/v2/services/haproxy/configuration/servers/$vmname"?"backend=poolweb"&"version=$version_ha -Method DELETE -Headers $headers
+    $Response.StatusCode
+}
+
+function deletevm_vsphere($vmarray)
+{
+    foreach ($vmname in $vmarray)
+    {
+        remove_from_ha($vmname)
+        Stop-VMGuest -VM $vmname -Confirm:$false
+    }
+    foreach ($vmname in $vmarray)
+    {
+        $stopped = $false
+        Do{
+            $vmtest = Get-VM -Name $vmname
+            $powerstatevm = $vmtest.PowerState
+        }Until($powerstatevm -eq 'PoweredOff')
+        Write-Output "Removing $vmname"
+        Remove-VM -VM $vmname -Confirm:$false
+    }
+}
+
+function downscale($vmarray)
+{
+    deletevm_vsphere($vmarray)
+}
+
+connect_vsphere
+
+downscale($VMs)
+
+disconnect_vsphere
---
+++ upscale.ps1
@@ -0,0 +1,98 @@
+﻿# Script principal contenant toutes les fonctions
+
+$VMs = 'g2-web-02','g2-web-03','g2-web-04'
+
+function connect_vsphere
+{
+    Write-Output "Connection to vSphere"
+    Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+    Connect-VIServer -Server 10.54.2.1 -Protocol https -User root -Password 'Pa$$w0rd'
+}
+
+function disconnect_vsphere
+{
+    Disconnect-VIServer -Server 10.54.2.1 -Confirm:$false
+}
+
+function getvm_ip($vmname)
+{
+    $success = $False
+    Do{
+        $ip_addr = (Get-VM -Name $vmname).Guest.IPAddress[0]
+        if([ipaddress]::TryParse($ip_addr,[ref][ipaddress]::Loopback) -eq $True)
+        {
+            $success = $True
+        }
+    }Until($success -eq $True)
+    $ip_addr
+}
+
+function createvm_vsphere($vmarray)
+{
+    foreach ($vmname in $vmarray)
+    {
+        New-VM -Name $vmname -VmFilePath "[datastore1] $vmname/$vmname.vmx"
+        Start-VM -VM $vmname
+    }
+}
+
+function is_iis_up($vmname)
+{
+    $ip = getvm_ip($vmname)
+    $status_code = $false
+    Do{
+        $Response = Invoke-WebRequest -Uri http://$ip -Headers @{"Host"="g2.eni"}
+        if ($Response.StatusCode -eq 200)
+        {
+            $status_code = $true
+        }
+        Start-Sleep -Seconds 5
+    }Until($status_code -eq $true)
+    $status_code
+}
+
+function get_ha_conf_version()
+{
+    $credentials_ha = "dataplaneapi:password"
+    $encoded_credentials = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($credentials_ha))
+    $headers = @{ Authorization = "Basic $encoded_credentials" }
+    $Response = Invoke-WebRequest -Uri http://10.54.2.2:5555/v2/services/haproxy/configuration/version -Method GET -Headers $headers
+    $version_ha = $Response.Content
+    $version_ha = $version_ha -as [int]
+    $version_ha
+}
+
+function add_to_ha($vmname)
+{
+    getvm_ip($vmname)
+    $version_ha = get_ha_conf_version
+    $params = @{"address"=getvm_ip($vmname);
+    "check"="enabled";
+    "name"="$vmname";
+    "port"=80
+    }
+    $credentials_ha = "dataplaneapi:password"
+    $encoded_credentials = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($credentials_ha))
+    $headers = @{ Authorization = "Basic $encoded_credentials" }
+    $Response = Invoke-WebRequest -Uri http://10.54.2.2:5555/v2/services/haproxy/configuration/servers?backend=poolweb"&"version=$version_ha -Method POST -Body ($params|ConvertTo-Json) -ContentType "application/json" -Headers $headers
+    $Response.StatusCode
+}
+
+function upscale($vmarray)
+{
+    createvm_vsphere($vmarray)
+    Start-Sleep 30
+    foreach ($vmname in $vmarray) 
+    {
+        if (is_iis_up($vmname) -eq 200)
+        {
+            add_to_ha($vmname)
+        }
+    }
+}
+
+connect_vsphere
+
+upscale($VMs)
+
+disconnect_vsphere
